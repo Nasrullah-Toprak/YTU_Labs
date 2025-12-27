0b305d881c1c444215f9b15da2d1ac7dbb9f9d41
Nasrullah Toprak <nasrtoprak446@gmail.com>
2025-12-27 11:36:40 +0300

    Refactor main.c for clarity and efficiency

--- Introduction to Computer Science/Lab 3/main.c
+++ Introduction to Computer Science/Lab 3/main.c
@@ -3,10 +3,7 @@
 
 int main()
 {
-    //N: ucaklarin sayisi - k: dakika araligi - i: for dongusunde kullanilacak degisken
     int N,k,i;
-
-    //N, k degerelerini kullanicidan talebi. Not N degeri pozitif ve sifirdan farki olmalidir:
     printf("Ucaklarin sayisini giriniz: ");
     scanf("%d",&N);
     while(N<=0){
@@ -17,10 +14,8 @@ int main()
     printf("\nIki ucak arasinda kalkis icin verilen dakika araligini giriniz: ");
     scanf("%d",&k);
 
-    //ucakno: girilen ucaklarin numaralari - klkzmn: ucaklarin kalkis zamanlari
     int ucakno[N], klkzmn[N];
 
-    //ucakno ve klkzmn dizilerin kullanicidan talebi:
     printf("\nUcaklarin numaralarini giriniz: \n");
     for(i=0;i<N;i++){
             scanf("%d",&ucakno[i]);
@@ -30,35 +25,21 @@ int main()
         scanf("%d",&klkzmn[i]);
     }
 
-    //klkzmn1: 1. piste atanan ucagin 1. pistten kalkis zamanini gosterir
-    //klkzmn2: 2. piste atanan ucagin 2. pistten kalkis zamanini gosterir
-    //sayac1: 1. pistin index
-    //sayac2: 2. pistin index
-    //gec1 1. pistteki ucagin kac dk gec kalkacagini gosterir
-    //gec2 2. pistteki ucagin kac dk gec kalkacagini gosterir
     int Pist1[N],klkzmn1[N],Pist2[N],klkzmn2[N];
     int sayac1=0,sayac2=0;
     double gec1=0, gec2=0;
 
-
-    // For Degiskeni:
     for(i=0;i<N;i++){
-        //Ilk ucak 1. pistten verilen dk ucus yapacak
         if(sayac1==0){
             Pist1[sayac1]=ucakno[i];
             klkzmn1[sayac1]=klkzmn[i];
             sayac1++;
         }
-        //Ikinci ucak 2. pistten verilen dk ucus yapacak
         else if(sayac2==0){
             Pist2[sayac2]=ucakno[i];
             klkzmn2[sayac2]=klkzmn[i];
             sayac2++;
         }
-        //Eger ucak bir onceki ucak ile en az k suresi ile ucus yapacaksa en uygun piste atama yapilmasi.
-		//Hangi iki pistte ucak daha once kalkiyorsa o piste atamak
-        //ve mumkun oldugu kadar sureyi ertelememek
-        //Eger bir sonraki ucak hemen ucacaksa (k suresinden daha az bir surede) o zaman ertelenir (bir alt kisimda)
         else if  (klkzmn[i] >= (klkzmn1[sayac1 - 1] + k) || klkzmn[i] >= (klkzmn2[sayac2 - 1] + k)) {
             if(klkzmn2[sayac2-1] > klkzmn1[sayac1-1]){
                 Pist1[sayac1]=ucakno[i];
@@ -83,7 +64,6 @@ int main()
                     }
                 }
             }
-        //k dakika araligindan daha az bir surede yapacak olan ucuslarin ertelenmesi ve gecikme dk (gec1 1. pistte gecikme ve gec2 2. pistte gecikme dakiklaridir) hesaplanmasi:
         else if (sayac1>sayac2){
             Pist2[sayac2]=ucakno[i];
             klkzmn2[sayac2]=klkzmn2[sayac2-1]+k;
@@ -96,17 +76,12 @@ int main()
             gec1 = gec1 + (klkzmn1[sayac1] - klkzmn[i]);
             sayac1++;
         }
-    } //for dongusu bitisi
-
-    // ortalama hesaplama (kesirli sayi oldugundan dolayi double kullandim)
+    } 
     double ort1, ort2;
     ort1 = gec1/(sayac1);
     if(sayac2 != 0){
         ort2 = gec2/(sayac2);
     }
-
-
-    // Sonuclari ekrana yazdirma
 
     printf("\nPist1_Ucaklar: ");
     for (i = 0; i < sayac1; i++) {
